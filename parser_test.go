983795575416abfd84f28916e2e9ab26215fa2c8
Elias Gill <eliasgill42@gmail.com>
2023-07-21 00:05:16 -0400

    bueno, iniciando de verdad y agregando gitignore

--- parser_test.go
+++ parser_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+    file, err := os.Open("/home/elias/Documentos/go_proyects/excelParser/example.xlsx")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+
+    _, err = excelize.OpenReader(file)
+    // cols, _ := data.GetCols(data.GetSheetName(6))
+}
---
+++ structs_vars.go
@@ -0,0 +1,93 @@
+package main
+
+type filaEncs struct {
+	fila    int
+	columna int
+    largo int // cantidad de materias
+}
+
+type Dias struct {
+	Lunes     string `json:"lunes"`
+    Martes    string `json:"martes"`
+    Miercoles string `json:"miercoles"`
+    Jueves    string `json:"jueves"`
+    Viernes   string `json:"viernes"`
+    Sabado    string `json:"sabado"`
+}
+
+type Materia struct {
+	// general
+	Id       string `json:"id"`
+	Nombre   string `json:"nombre"`
+	Semestre string `json:"semestre"`
+	Seccion  string `json:"seccion"`
+	Profesor string `json:"profesor"`
+	// examenes
+	Parcial1 string `json:"parcial_1"`
+	Parcial2 string `json:"parcial_2"`
+	Final1   string `json:"final_1"`
+	Final2   string `json:"final_2"`
+	// horario de clase
+	Dias Dias `json:"dias"`
+}
+
+type examen struct {
+    fecha string
+    hora string
+}
+
+type Carrera struct {
+	Asignaturas []*Materia `json:"Asignaturas"`
+	Nombre      string     `json:"nombre"`
+	Index       int        `json:"index"`
+}
+
+const (
+    asignatura = iota
+    item
+    dpto
+    nivel
+    seccion
+    apellido
+    nombre
+    correo
+    dia
+    hora
+    lunes
+    martes
+    miercoles
+    jueves
+    viernes
+    sabado
+    final1
+    final2
+    parcial1
+    parcial2
+)
+var (
+	filaEncabezados filaEncs
+
+    // mapeo de encabezados a constantes
+	encabezados     = map[string]int{
+        "Asignatura": asignatura,
+        "Item": item,
+        "DPTO": dpto,
+        "Grupo": nivel,
+        "Sección": seccion,
+        "Apellido": apellido,
+        "Nombre": nombre,
+        "Correo": correo,
+        "Día": dia,
+        "Hora": hora,
+        "Lunes": lunes,
+        "Martes": martes,
+        "Miércoles": miercoles,
+        "Jueves": jueves,
+        "Viernes": viernes,
+        "Sábado": sabado,
+        "1er. Parcial": parcial1,
+        "2do. Parcial": parcial2,
+        "1er. Final": final1,
+        "2do. Final": final2,
+	}
+)
