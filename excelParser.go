cdf8a1cf01e6aba6e690a2acf572bfb848b7bc91
Elias Gill <eliasgill42@gmail.com>
2023-08-05 19:45:36 -0400

    feat(parser): Adding support for io.reader

--- excelParser.go
+++ excelParser.go
@@ -3,7 +3,7 @@ package excelParser
 import "github.com/xuri/excelize/v2"
 
 // funcion principal de la libreria
-func ParsearArchivo(file string) ([]Carrera, error) {
+func ParseFileFromName(file string) ([]Carrera, error) {
 	f, err := excelize.OpenFile(file)
 	if err != nil {
 		return nil, err
@@ -32,4 +32,29 @@ func ParsearArchivo(file string) ([]Carrera, error) {
 	}
 
 	return res, nil
+}
+
+func ParseFileFromIo(file *excelize.File) ([]Carrera, error) {
+    var res []Carrera
+    sheets := file.GetSheetList()
+    // parsear todas las hojas
+    for i := 1; i < len(sheets); i++ {
+        cols, err := file.GetCols(sheets[i])
+        if err != nil {
+            return nil, err
+        }
+
+        encs, err := encontrarFilaEncabezados(cols)
+        if err != nil {
+            return nil, err
+        }
+
+        materias := parsearListaMaterias(cols, encs)
+        res = append(res, Carrera{
+            Asignaturas: materias,
+            Nombre:      sheets[i],
+            Index:       i})
+    }
+
+    return res, nil
 }
